3e6f6389cd1a3c3ebfeb20f4e3a80fee0e0a80da
Valerii_Panov <val250640@mail.ru>
2024-03-02 16:27:36 +1000

    hw11

---
+++ Homework06/hospital.sql
@@ -0,0 +1,20 @@
+ALTER TABLE `patient` DROP FOREIGN KEY `patient_fk0`;
+
+ALTER TABLE `patient` DROP FOREIGN KEY `patient_fk1`;
+
+ALTER TABLE `policy` DROP FOREIGN KEY `policy_fk0`;
+
+ALTER TABLE `doctor` DROP FOREIGN KEY `doctor_fk0`;
+
+ALTER TABLE `doctor` DROP FOREIGN KEY `doctor_fk1`;
+
+ALTER TABLE `doctor` DROP FOREIGN KEY `doctor_fk2`;
+
+ALTER TABLE `post` DROP FOREIGN KEY `post_fk0`;
+
+ALTER TABLE `schedule` DROP FOREIGN KEY `schedule_fk0`;
+
+ALTER TABLE `schedule` DROP FOREIGN KEY `schedule_fk1`;
+
+ALTER TABLE `schedule` DROP FOREIGN KEY `schedule_fk2`;
+
